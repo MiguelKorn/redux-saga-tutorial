5731fdb703b3f06027b26255d3b006d2f790250c
Miguel Korn <20448@ma-web.nl>
2017-11-29 14:56:27 +0100

    added action tests

---
+++ src/__tests__/actions.js
@@ -0,0 +1,44 @@
+import * as counterActions from '../actions/counterActions';
+import * as mediaActions from '../actions/mediaActions';
+import * as types from '../constants/actionTypes';
+
+describe('actions', () => {
+    describe('media', () => {
+        it('should create an action to return search query', () => {
+            const payload = 'rain';
+            const expectedAction = {
+                type: types.SEARCH_MEDIA_REQUEST,
+                payload
+            };
+            expect(mediaActions.searchMediaAction(payload)).toEqual(expectedAction);
+        });
+
+        it('should create an action to return an image', () => {
+            const image = {};
+            const expectedAction = {
+                type: types.SELECTED_IMAGE,
+                image
+            };
+            expect(mediaActions.selectImageAction(image)).toEqual(expectedAction);
+        });
+
+        it('should create an action to return an video', () => {
+            const video = {};
+            const expectedAction = {
+                type: types.SELECTED_VIDEO,
+                video
+            };
+            expect(mediaActions.selectVideoAction(video)).toEqual(expectedAction);
+        });
+    });
+
+    describe('counter', () => {
+        it('should create an action to increment the counter', () => {
+            expect(counterActions.counterIncrement()).toEqual({type: types.COUNTER_INCREMENT});
+        });
+
+        it('should create an action to decrement the counter', () => {
+            expect(counterActions.counterDecrement()).toEqual({type: types.COUNTER_DECREMENT});
+        });
+    });
+});
