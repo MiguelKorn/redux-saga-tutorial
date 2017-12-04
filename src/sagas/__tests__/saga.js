e90ea511b09849485642fab2433b2161b4e9b4b9
Miguel Korn <20448@ma-web.nl>
2017-12-04 12:49:32 +0100

    fixed media saga test

--- src/sagas/__tests__/saga.js
+++ src/sagas/__tests__/saga.js
@@ -2,23 +2,32 @@ import { incrementAsync } from '../counterSaga';
 import * as types from '../../constants/actionTypes';
 import { delay } from 'redux-saga';
 import { call, put } from 'redux-saga/effects';
+import { expectSaga } from 'redux-saga-test-plan';
 import { searchMediaSaga } from "../mediaSaga";
-import { flickrImages } from "../../api/api";
-
+import { flickrImages, shutterStockVideos } from "../../api/api";
 
 describe('sagas', () => {
-    // describe('media', () => {
-    //     it('should call data from the api', () => {
-    //         const action = {payload: 'rain'};
-    //         const gen = searchMediaSaga(action);
-    //         const expectedYield = call(flickrImages, action.payload);
-    //
-    //         expect(expectedYield).toEqual(gen.next().value);
-    //
-    //         const images = {};
-    //         expect(put({type: types.FLICKR_IMAGES_SUCCESS, images})).toEqual(gen.next(images).value);
-    //     });
-    // });
+    describe('media', () => {
+        it('should call data from the api', () => {
+            const action = {payload: "rain"};
+            const images = [{
+                id: "38111730024",
+                title: "How Many Gallons in Lake Michigan and Other Facts",
+                mediaUrl: "https://farm5.staticflickr.com/4563/38111730024_ec2c6dbe10.jpg"
+            }];
+            const videos = [{}];
+
+
+            return expectSaga(searchMediaSaga, action)
+                .provide([
+                    [call(shutterStockVideos, action.payload), videos],
+                    [call(flickrImages, action.payload), images],
+                ])
+                .put({type: types.FLICKR_IMAGES_SUCCESS, images})
+                .dispatch({type: types.SEARCH_MEDIA_REQUEST, payload: 'rain'})
+                .run()
+        });
+    });
 
     describe('counter', () => {
         it('should increment counter after 1 sec', () => {
@@ -28,7 +37,7 @@ describe('sagas', () => {
 
             expect(expectedYield).toEqual(gen.next().value);
 
-            expect(put({type:types.COUNTER_INCREMENT})).toEqual(gen.next().value);
+            expect(put({type: types.COUNTER_INCREMENT})).toEqual(gen.next().value);
         });
     });
 });
