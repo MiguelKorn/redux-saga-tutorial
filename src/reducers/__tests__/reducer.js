8c997be7e381d9a8720d7471dcadfc17d51baed2
Miguel Korn <20448@ma-web.nl>
2017-12-05 12:12:55 +0100

    formatted code

--- src/reducers/__tests__/reducer.js
+++ src/reducers/__tests__/reducer.js
@@ -48,35 +48,31 @@ describe('reducers', () => {
         });
 
         it('should handle SELECTED_IMAGE', () => {
-            expect(
-                imageReducer([], {
-                    type: types.SELECTED_IMAGE,
-                    image: {
-                        id: 1,
-                        name: 'image'
-                    }
-                })
-            ).toEqual({
+            expect(imageReducer([], {
+                type: types.SELECTED_IMAGE,
+                image: {
+                    id: 1,
+                    name: 'image'
+                }
+            })).toEqual({
                 selectedImage: {
                     id: 1,
                     name: 'image'
                 }
             });
 
-            expect(
-                imageReducer({
-                    selectedImage: {
-                        id: 1,
-                        name: 'image'
-                    }
-                }, {
-                    type: types.SELECTED_IMAGE,
-                    image: {
-                        id: 2,
-                        name: 'image2'
-                    }
-                })
-            ).toEqual({
+            expect(imageReducer({
+                selectedImage: {
+                    id: 1,
+                    name: 'image'
+                }
+            }, {
+                type: types.SELECTED_IMAGE,
+                image: {
+                    id: 2,
+                    name: 'image2'
+                }
+            })).toEqual({
                 selectedImage: {
                     id: 2,
                     name: 'image2'
@@ -85,14 +81,13 @@ describe('reducers', () => {
         });
 
         it('should handle FLICKR_IMAGES_SUCCESS', () => {
-            expect(
-                imageReducer([], {
-                    type: types.FLICKR_IMAGES_SUCCESS,
-                    images: [
-                        {id: 1, name: 'image1'},
-                        {id: 2, name: 'image2'}
-                    ]
-                })).toEqual([[
+            expect(imageReducer([], {
+                type: types.FLICKR_IMAGES_SUCCESS,
+                images: [
+                    {id: 1, name: 'image1'},
+                    {id: 2, name: 'image2'}
+                ]
+            })).toEqual([[
                     {id: 1, name: 'image1'},
                     {id: 2, name: 'image2'}
                 ]]
@@ -106,51 +101,31 @@ describe('reducers', () => {
         });
 
         it('should handle SELECTED_VIDEO', () => {
-            expect(
-                videoReducer([], {
-                    type: types.SELECTED_VIDEO,
-                    video: {
-                        id: 1,
-                        name: 'video'
-                    }
-                })
-            ).toEqual({
-                selectedVideo: {
-                    id: 1,
-                    name: 'video'
-                }
+            expect(videoReducer([], {
+                type: types.SELECTED_VIDEO,
+                video: {id: 1, name: 'video'}
+            })).toEqual({
+                selectedVideo: {id: 1, name: 'video'}
             });
 
-            expect(
-                videoReducer({
-                    selectedVideo: {
-                        id: 1,
-                        name: 'video'
-                    }
-                }, {
-                    type: types.SELECTED_VIDEO,
-                    video: {
-                        id: 2,
-                        name: 'video2'
-                    }
-                })
-            ).toEqual({
-                selectedVideo: {
-                    id: 2,
-                    name: 'video2'
-                }
+            expect(videoReducer({
+                selectedVideo: {id: 1, name: 'video'}
+            }, {
+                type: types.SELECTED_VIDEO,
+                video: {id: 2, name: 'video2'}
+            })).toEqual({
+                selectedVideo: {id: 2, name: 'video2'}
             });
         });
 
         it('should handle SHUTTER_VIDEOS_SUCCESS', () => {
-            expect(
-                videoReducer([], {
-                    type: types.SHUTTER_VIDEOS_SUCCESS,
-                    videos: [
-                        {id: 1, name: 'video1'},
-                        {id: 2, name: 'video2'}
-                    ]
-                })).toEqual([[
+            expect(videoReducer([], {
+                type: types.SHUTTER_VIDEOS_SUCCESS,
+                videos: [
+                    {id: 1, name: 'video1'},
+                    {id: 2, name: 'video2'}
+                ]
+            })).toEqual([[
                     {id: 1, name: 'video1'},
                     {id: 2, name: 'video2'}
                 ]]
