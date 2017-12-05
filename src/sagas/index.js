345d6364eeb90be6b43a073d846daded069f2095
Miguel Korn <20448@ma-web.nl>
2017-12-05 11:01:38 +0100

    combined watcher.js with index

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,10 +1,12 @@
-import {fork, all} from 'redux-saga/effects';
-import {watchSearchMedia, watchIncrementAsync} from './watcher';
+import { fork, all, takeLatest } from 'redux-saga/effects';
+import { searchMediaSaga } from "./mediaSaga";
+import { incrementAsync } from "./counterSaga";
+import * as types from "../constants/actionTypes";
 
 // register sagas and export as a single generator
 export default function* startForeman() {
-    yield all([
-        fork(watchSearchMedia),
-        fork(watchIncrementAsync)
-    ]);
+    // yield all([
+    yield fork(takeLatest, types.SEARCH_MEDIA_REQUEST, searchMediaSaga);
+    yield fork(takeLatest, types.COUNTER_INCREMENT_ASYNC, incrementAsync);
+    // ]);
 }
